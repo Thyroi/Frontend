7627e4dfc78e115d4448f51736a605626040e75b
Pablo Rovito <pablo.rovito@outlook.com>
2022-03-29 14:01:46 -0300

    transfer fixed repo
    copy new version of dev into my branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Landing from './components/Landing/Landing';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
         <Landing/>
     </div>
   );
+=======
+export default function App() {
+	return (
+		<div className='App'>
+			<h1>Aguante Diego Rodriguez y Fano y Romi y Pablo y Sneyder</h1>
+			<Landing />
+		</div>
+	);
+>>>>>>> Stashed changes
 }
-
-export default App;
--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,7 +1,21 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 export default function Landing() {
   return (
     <div>Landing</div>
   )
+=======
+import React from 'react';
+import styles from 'Landing.module.css';
+
+export default function Landing() {
+	return (
+		<div className={styles.global}>
+			<div className={styles.top}>Top</div>
+			<div className={styles.left}>Left</div>
+			<div className={styles.container}>Site</div>
+		</div>
+	);
+>>>>>>> Stashed changes
 }

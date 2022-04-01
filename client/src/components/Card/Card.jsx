bc521f1f145f420a218ffc82fd23984108e8c9b5
Pablo Rovito <pablo.rovito@outlook.com>
2022-04-01 20:34:40 -0300

    cards fixed
    minor changes in style

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom';
 import style from './Card.module.css';
 
 export default function Card({ data }) {
-	let stocks =
-		'Units: 20'; /* stocks debería sumar todos los stocks de todos los talles y eso... */
-
 	const {
 		id_product,
 		name,
@@ -16,25 +13,24 @@ export default function Card({ data }) {
 		variants,
 	} = data;
 
+	let stocks =
+		'Units: 20'; /* stocks debería sumar todos los stocks de todos los talles y eso... */
+
 	return (
 		<div className={style.container}>
-			<Link to={`/detail/${id_product}`}>
-				<div className={style.background}>
+			<div className={style.background}>
+				<Link to={`/detail/${id_product}`}>
 					{is_offer && (
 						<span className={style.offer}>{'Oferta'}</span>
 					)}
 					<img src={default_image} alt='' />
-				</div>
-			</Link>
+				</Link>
+			</div>
 			<div className={style.info}>
 				<div className={style.name}>{name}</div>
-
 				<div className={style.brand}>{brand}</div>
-
 				<div className={style.price}>{price}</div>
-
 				{/* falta colección que no está aún en el json creo */}
-
 				<div className={style.stock}>{stocks}</div>
 			</div>
 		</div>

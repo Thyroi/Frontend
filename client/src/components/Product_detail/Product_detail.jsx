ef40ed5aaf97343612711777a9f871926dee4b19
Esneider Ospina <sneuder8@gmail.com>
2022-04-01 11:33:39 -0500

    Classes added for styles

--- client/src/components/Product_detail/Product_detail.jsx
+++ client/src/components/Product_detail/Product_detail.jsx
@@ -1,64 +1,80 @@
 import React from "react";
 import style from "./Product_detail.module.css";
 
+// Waiting for routes and data to deploy it finally
+
 function Product_detail() {
   return (
-    <div>
-      <div>
-        <div>
-          <img />
+    <div className={style.container}>
+      <div className={style.containerImages}>
+        <div className={style.containerMainImage}>
+          <img className={style.mainImage} />
         </div>
-        <div>
-          <img />
-          <img />
-          <img />
-          <img />
+        <div className={style.containerSecondImages}>
+          <img className={style.secondImages} />
+          <img className={style.secondImages} />
+          <img className={style.secondImages} />
+          <img className={style.secondImages} />
         </div>
       </div>
 
-      <div>
-        <div>
-          <h2>Flannel Shirt Dark</h2>
-          <p>Charles Jeffrey LOVERBOY, Otoño-invierno 2017</p>
-          <p>$450</p>
-          <div>
-            <h3>Size</h3>
-            <div>
-              <div>S</div>
-              <div>M</div>
-              <div>L</div>
-              <div>XL</div>
+      <div className={style.containerInf}>
+        <div className={style.specificInf}>
+          <h2 className={style.productName}>Flannel Shirt Dark</h2>
+          <p className={style.collectionName}>
+            Charles Jeffrey LOVERBOY, Otoño-invierno 2017
+          </p>
+          <p className={style.productPrice}>$450</p>
+          <div className={style.containerPreferences}>
+            <div className={style.containerSizePreference}>
+              <h3 className={style.sizeHeader}>Size</h3>
+              <div className={style.sizes}>
+                <div className={style.size}>S</div>
+                <div className={style.size}>M</div>
+                <div className={style.size}>L</div>
+                <div className={style.size}>xL</div>
+              </div>
             </div>
-            <div>
-              <div>blue</div>
-              <div>red</div>
-              <div>black</div>
-              <div>green</div>
+
+            <div className={style.containerColorPreference}>
+              <h3 className={style.colorSize}>Color</h3>
+              <div className={style.colors}>
+                <div className={style.color}>blue</div>
+                <div className={style.color}>red</div>
+                <div className={style.color}>black</div>
+                <div className={style.color}>green</div>
+              </div>
             </div>
-            <div>
+
+            <div className={style.containerAmountFavorite}>
               <div className="counterContainer">
                 <button>-</button>
                 <p>2</p>
                 <button>+</button>
               </div>
-              <div>
-                <img src="" alt="favorite"/>
+              <div className={style.favorite}>
+                <img className={style.favoriteImg} src="" alt="favorite" />
               </div>
             </div>
-            <div>
-              <button>Buy</button>
-              <button><img src="" alt="" /></button>                
+
+            <div className={style.containerBuyCart}>
+              <button className={style.buyButton}>Buy</button>
+              <button className={style.cartButton}>
+                <img src="" alt="" />
+              </button>
             </div>
-            <div>
-              <p>Available Units: <span>58</span></p>
+            <div className={style.containerUnits}>
+              <p className={style.infoUnits}>
+                Available Units: <span className={style.units}>58</span>
+              </p>
             </div>
           </div>
         </div>
 
-        <div>
-          <h4>Description</h4>
-          <hr />
-          <p>
+        <div className={style.generalInformation}>
+          <h4 className={style.headerDescription}>Description</h4>
+          <hr className={style.line} />
+          <p className={style.description}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
             dolores animi sint repellendus facere id, laudantium odio ullam, ea
             excepturi mollitia quos eius? Reiciendis qui fugiat laudantium,

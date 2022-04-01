baf07eeef05ddd014c7072832f736c1c21dd69bc
Pablo Rovito <pablo.rovito@outlook.com>
2022-03-31 22:32:15 -0300

    Landing+CardCategory components
    Did template of components, there are no functionalities and some data is hardcoded

---
+++ client/src/components/CardCategory/CardCategory.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './CardCategory.module.css';
+
+function CardCategory({ img, name }) {
+	return (
+		<div className={styles.container}>
+			{
+				{
+					/* en la class padre voy a poner la img de background */
+				}
+			}
+			<div>
+				<Link to={`/category?name=${name}`}>{name}</Link>
+			</div>
+		</div>
+	);
+}
+
+export default CardCategory;

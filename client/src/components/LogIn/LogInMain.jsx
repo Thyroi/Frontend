87c2c8b15a78b2254eda1bec04459aeac5080fb8
Esneider Ospina <sneuder8@gmail.com>
2022-04-02 16:07:15 -0500

    Built component

--- client/src/components/LogIn/LogInMain.jsx
+++ client/src/components/LogIn/LogInMain.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function LogInMain() {
   return (
-    <div>LogInMain</div>
-  )
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <div>
+            <div>
+              <h1>Welcome back!</h1>
+              <p>It's great to have you back!</p>
+            </div>
+
+            <div>
+              <label>Username</label>
+              <input type="text" />
+            </div>
+
+            <div>
+              <label>Password</label>
+              <input type="password" />
+            </div>
+
+            <div>
+              <div>
+                <input type="checkbox" />
+                <label>Remember me?</label>
+              </div>
+
+              <p>Forgot password?</p>
+            </div>
+
+            <div>
+              <input type="submit" value="Login"/>
+              <button>Sign Up</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LogInMain
+export default LogInMain;

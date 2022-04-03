d36c82d608e33976e5dbab9702756f08c3349f2c
Esneider Ospina <sneuder8@gmail.com>
2022-04-03 10:15:36 -0500

    Adding classNames for styles components

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,60 +1,62 @@
-import React from 'react';
-import style from "./Cart.module.css";
+import React from "react";
+import style from "./Cart.module.scss";
 //To add context below here
 
 // It is much better to add events listeners to keep a clean code
 // Take into consideration the stock when increasing or decreasing amount
 
-function Cart(){
+function Cart() {
   // Provisional
   const itemsCart = ["Clothe", "Clothe", "Clothe", "Clothe"];
 
-  return(
-    <div className="containerCart">
-      {
-        itemsCart.map(item => {
-          return(
-            <div className="itemContainer">
-              <div className='imgContainer'>
-                <img src="" alt="iconOff" />
-                <img src="" alt="product" />
-              </div>
+  return (
+    <div className={style.containerCart}>
+      {itemsCart.map((item) => {
+        return (
+          <div className={style.itemContainer}>
+            <div className={style.imgContainer}>
+              <img src="" alt="iconOff" />
+              <img src="" alt="product" />
+            </div>
 
-              <div className="infoContainer">
-                <h3>Name Product</h3>
-                <p>Product Price</p>
+            <div className={style.infoContainer}>
+              <div className={style.subInfoContainer}>
+                <h3 className={style.nameProduct}>Name Product</h3>
+                <p className={style.productPrice}>Product Price</p>
 
-                <div className="itemButtons">
-                  <div className="counterContainer">
-                    <button>-</button>
-                    <p>2</p>
-                    <button>+</button>
+                <div className={style.counterContainer}>
+                  <button className={style.counterButton}>-</button>
+                  <p className={style.counterNumber}>2</p>
+                  <button className={style.counterButton}>+</button>
+                </div>
+
+                <div className={style.containerButtons}>
+                  <div className={style.containerDiscount}>
+                    <input className={style.inputDiscount} type="text" placeholder="Discount Code" />
+                    <input className={style.applyDiscount} type="submit" value="Apply" />
                   </div>
 
-                  <div className="containerButtons">
-                    <div className="containerDiscount">
-                      <input type="text" placeholder="Discount Code"/>
-                      <input type="submit" value="Apply" />
-                    </div>
-                    <button className="removeButton">
-                      <img src="" alt="" />
-                      <p>Remove</p>
-                    </button>
-                  </div>
-
+                  <button className={style.removeButton}>
+                    <img src="" alt="" />
+                    <p>Remove</p>
+                  </button>
                 </div>
               </div>
             </div>
-          )
-        })
-      }
+          </div>
+        );
+      })}
 
-      <div className="purchaseContainer">
-        <p>Total due <span className="totalPrice">900</span></p>
-        <button className="buyButton" onClick={console.log("working")}>Buy</button>
+      <div className={style.purchaseContainer}>
+        <p className={style.totalInfo}>
+          Total due: <span className={style.totalPrice}>$900</span>
+        </p>
+        <button className={style.buyButton}>
+          Buy
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Cart;

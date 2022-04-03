2f68511e69637d406282ddd328ec8c71efb696ab
Esneider Ospina <sneuder8@gmail.com>
2022-04-02 19:15:44 -0500

    Adding classes and links

--- client/src/components/LogIn/LogInMain.jsx
+++ client/src/components/LogIn/LogInMain.jsx
@@ -1,41 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import style from "./LoginMain.module.scss";
 
 function LogInMain() {
   return (
-    <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div className={style.background}>
+      <div className={style.container}>
+        <div className={style.containerImage}>
+          <img
+            className={style.img}
+            src={require("../../Assets/img/login_side.jpg")}
+            alt=""
+          />
         </div>
-        <div>
-          <div>
-            <div>
-              <h1>Welcome back!</h1>
-              <p>It's great to have you back!</p>
+        <div className={style.containerForm}>
+          <div className={style.subContainerForm}>
+            <div className={style.containerHeader}>
+              <h1 className={style.header}>Welcome back!</h1>
+              <p className={style.subHeader}>It's great to have you back!</p>
             </div>
 
-            <div>
-              <label>Username</label>
-              <input type="text" />
+            <div className={style.containerInput}>
+              <label className={style.labelInput}>Username</label>
+              <input className={style.input} type="text" placeholder="Type username"/>
             </div>
 
-            <div>
-              <label>Password</label>
-              <input type="password" />
+            <div className={style.containerInput}>
+              <label className={style.labelInput}>Password</label>
+              <input className={style.input} type="password" placeholder="Type password"/>
             </div>
 
-            <div>
-              <div>
-                <input type="checkbox" />
-                <label>Remember me?</label>
+            <div className={style.containerOptions}>
+              <div className={style.containerRememberMe}>
+                <input className={style.buttonRemember} type="checkbox" />
+                <label className={style.textRemember}>Remember me?</label>
               </div>
 
-              <p>Forgot password?</p>
+              <Link className={style.forgotText} to="/retrievepassword">Forgot password?</Link>
             </div>
 
-            <div>
-              <input type="submit" value="Login"/>
-              <button>Sign Up</button>
+            <div className={style.containerLoginSignUp}>
+              <input className={style.loginButton} type="submit" value="Login" />
+              <Link className={style.signUpButton} to="/signup">Sign Up</Link>
             </div>
           </div>
         </div>

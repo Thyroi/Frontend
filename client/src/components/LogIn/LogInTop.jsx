d0b28c9c17e358d0612d2f941867d46a73c176de
Esneider Ospina <sneuder8@gmail.com>
2022-04-05 16:43:42 -0500

    Styles for Login button at Top

--- client/src/components/LogIn/LogInTop.jsx
+++ client/src/components/LogIn/LogInTop.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from './LoginTop.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser  } from "@fortawesome/free-solid-svg-icons";
+
 function LogInTop() {
 	return (
-		<div>
-			<Link to='/LogIn'>LogInTop</Link>
+		<div className={styles.loginContainer}>
+			<Link className={styles.text} to='/LogIn'>LogIn</Link>
+      <FontAwesomeIcon icon={faCircleUser} className={styles.icon} />
 		</div>
 	);
 }

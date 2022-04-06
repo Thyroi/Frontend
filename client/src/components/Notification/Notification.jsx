e050fc19becf6416e542d11cb3ffec60ac876f17
Esneider Ospina <sneuder8@gmail.com>
2022-04-05 20:40:31 -0500

    Icon for close notification function

--- client/src/components/Notification/Notification.jsx
+++ client/src/components/Notification/Notification.jsx
@@ -1,11 +1,16 @@
 import { React } from "react";
 import style from "./Notification.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
 function Notification() {
   return (
     <div className={style.noNotification} id="notification">
       <p className={style.text} id="textNotification">Product added to cart</p>
-      <button className={style.closeButton}>X</button>
+      <button className={style.closeButton}>
+        <FontAwesomeIcon className={style.closeIcon} icon={faXmark} />
+      </button>
     </div>
   );
 }

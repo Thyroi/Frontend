a74e59a4c6639da196e5c3c09266dda6fa4d235c
Pablo Rovito <pablo.rovito@outlook.com>
2022-04-08 12:27:03 -0300

    relevant images + improved layout

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -6,27 +6,27 @@ export default function Landing() {
 	let categories = [
 		{
 			id: 0,
-			img: 'https://media.cntraveler.com/photos/61eae29a14715b6c92bc1dd1/master/w_1600%2Cc_limit/Maldives_GettyImages-686525938.jpg',
+			img: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/721/837/products/20201111_1705151-cee01b48556482841516052164004281-640-0.jpg',
 			name: 'Summer',
 		},
 		{
 			id: 1,
-			img: 'https://cdn.britannica.com/88/137188-050-8C779D64/Boston-Public-Garden.jpg',
+			img: 'https://www.ubuy.com.bh/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNzEzS0VpTE50Q0wuX0FDX1VMMTUwMF8uanBn.jpg',
 			name: 'Autumn',
 		},
 		{
 			id: 2,
-			img: 'https://thumbs.dreamstime.com/b/winter-mountain-landscape-clear-blue-sky-over-snowy-mountain-peaks-frosty-morning-winter-sunrise-mountains-christmas-131610926.jpg',
+			img: 'https://m.media-amazon.com/images/I/7181mldnipL._AC_UX342_.jpg',
 			name: 'Winter',
 		},
 		{
 			id: 3,
-			img: 'https://479g5s2lzyvpzbbe4encn113-wpengine.netdna-ssl.com/triangle/wp-content/uploads/sites/61/2019/03/spring-allergie.jpg',
+			img: 'https://www.review-australia.com/dw/image/v2/BBBB_PRD/on/demandware.static/-/Sites-review-master-catalog/default/dw495fda72/images/RE20DR323_REGRN087_5.jpg',
 			name: 'Spring',
 		},
 		{
 			id: 4,
-			img: 'https://i.pinimg.com/originals/65/5a/ef/655aefa16482ad8604e5c34b93de9cf4.jpg',
+			img: 'https://s3.envato.com/files/335455718/160_1R3A9771.jpg',
 			name: 'All',
 		},
 	];

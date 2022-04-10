b4b7d1b4eb39ec0f04605d9a61bfa73ac313356e
Juan José Salgado Niño <thyroi0208@gmail.com>
2022-04-10 11:09:34 -0500

    Solving warnings

--- client/src/components/LogIn/LogInMain.jsx
+++ client/src/components/LogIn/LogInMain.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import style from "./LoginMain.module.scss";
 import { GoogleLogin } from 'react-google-login';

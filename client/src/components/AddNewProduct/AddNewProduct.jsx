bd9a175c7bb3741fa807ba3817cd66b30bab6eb2
Romina Bocon <63875657+rominabocon@users.noreply.github.com>
2022-04-10 13:24:28 -0300

    SolvingIDincrementAddProduct

--- client/src/components/AddNewProduct/AddNewProduct.jsx
+++ client/src/components/AddNewProduct/AddNewProduct.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import styles from './AddNewProduct.module.css';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { getSelectorsCat, addProduct } from '../../actions';
+import { getSelectorsCat, addProduct, getInfo } from '../../actions';
 import { storage } from '../../Assets/firebase';
 import { v4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,11 +12,14 @@ export default function AddNewProduct() {
 	const [imageUrls, setImageUrls] = useState([]);
 	const dispatch = useDispatch();
 	const categorias = useSelector((state) => state.categories);
+	const productos = useSelector((state) => state.products);
 
 	useEffect(() => {
 		dispatch(getSelectorsCat());
+		dispatch(getInfo())
 	}, [dispatch]);
-
+	
+	
 	let mujeres = categorias?.women;
 	let hombres = categorias?.men;
 
@@ -42,7 +45,7 @@ export default function AddNewProduct() {
 		data.price = parseInt(data.price);
 		data.categories = [data.categories];
 		data.default_image = imageUrls[0];
-		data.id_product = 10;
+		data.id_product = productos.length+1;
 		data.variants[0].ProductImages=[imageUrls[0]]
 		data.variants[0].Stocks.L = parseInt(data.variants[0].Stocks.L);
 		data.variants[0].Stocks.M = parseInt(data.variants[0].Stocks.M);

340f2d972c2a8ab7d0af544b088d7e62676511fc
Esneider Ospina <sneuder8@gmail.com>
2022-04-14 16:24:10 -0500

    Styles for profile component

--- client/src/components/Client_profile/Client_profile.jsx
+++ client/src/components/Client_profile/Client_profile.jsx
@@ -1,68 +1,98 @@
 import React from "react";
 import style from "./Client_profile.module.scss";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faWallet,
+  faBagShopping,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
-function ClientProfile(){
+function ClientProfile() {
   return (
     <div className={style.background}>
       <div className={style.firstColumn}>
         <div className={style.containerForm}>
           <form className={style.subContainerForm}>
-
             <div className={style.formGroups}>
               <div className={style.inputContainer}>
                 <label className={style.titleInput}>User</label>
                 <input className={style.input} type="text" />
               </div>
               <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
+                <label className={style.titleInput}>Password</label>
                 <input className={style.input} type="text" />
               </div>
             </div>
 
             <div className={style.formGroups}>
               <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
+                <label className={style.titleInput}>Name</label>
                 <input className={style.input} type="text" />
               </div>
               <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
-                <input className={style.input} type="text" />
-              </div>
-            </div>
-
-            <div className={style.formGroups}>
-              <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
-                <input className={style.input} type="text" />
-              </div>
-              <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
+                <label className={style.titleInput}>Last name</label>
                 <input className={style.input} type="text" />
               </div>
             </div>
 
             <div className={style.formGroups}>
               <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
+                <label className={style.titleInput}>Age</label>
                 <input className={style.input} type="text" />
               </div>
               <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
-                <input className={style.input} type="text" />
-              </div>
-            </div>
-
-            <div className={style.formGroups}>
-              <div className={style.inputContainer}>
-                <label className={style.titleInput}>User</label>
+                <label className={style.titleInput}>DNI</label>
                 <input className={style.input} type="text" />
               </div>
             </div>
 
             <div className={style.formGroups}>
               <div className={style.inputContainer}>
-                <input className={style.saveDataButton} type="submit" value="Save data" />
+                <label className={style.titleInput}>Phone</label>
+                <input className={style.input} type="text" />
+              </div>
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>Email</label>
+                <input className={style.input} type="text" />
+              </div>
+            </div>
+
+            <div className={style.formGroups}>
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>Street</label>
+                <input className={style.input} type="text" />
+              </div>
+
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>State</label>
+                <input className={style.input} type="text" />
+              </div>
+
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>City</label>
+                <input className={style.input} type="text" />
+              </div>
+
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>Zip code</label>
+                <input className={style.input} type="text" />
+              </div>
+
+              <div className={style.inputContainer}>
+                <label className={style.titleInput}>Other details</label>
+                <input className={style.input} type="text" />
+              </div>
+            </div>
+
+            <div className={style.formGroups}>
+              <div className={style.inputContainerSave}>
+                <input
+                  className={style.saveDataButton}
+                  type="submit"
+                  value="Save data"
+                />
               </div>
             </div>
           </form>
@@ -70,24 +100,39 @@ function ClientProfile(){
 
         <div className={style.infoContainer}>
           <div className={style.totalSpendContainer}>
-
+            <FontAwesomeIcon className={style.infoIconW} icon={faWallet} />
+            <div className={style.textInfo}>
+              <p className={style.textInfoTitle}>Total spend</p>
+              <p className={style.textInfoContent}>$600000</p>
+            </div>
           </div>
+
           <div className={style.totalPurchaseContainer}>
-
+            <FontAwesomeIcon className={style.infoIconB} icon={faBagShopping} />
+            <div className={style.textInfo}>
+              <p className={style.textInfoTitle}>Total purchase</p>
+              <p className={style.textInfoContent}>$600000</p>
+            </div>
           </div>
-          <div className={style.deleteAccountContainer}>
 
+          <div className={style.deleteAccountContainer}>
+            <FontAwesomeIcon className={style.infoIconC} icon={faTrashCan} />
+            <p className={style.deleteText}>Delete account</p>
           </div>
         </div>
-
       </div>
+
       <div className={style.secondColumn}>
         <div className={style.profileImageContainer}>
-          <img className={style.profileImage} src="" alt="profile" />
+          <img
+            className={style.profileImage}
+            src="https://ichef.bbci.co.uk/news/624/amz/worldservice/live/assets/images/2015/05/11/150511213032_sp_bin_laden_624x351_ap.jpg"
+            alt="profile"
+          />
         </div>
 
         <div className={style.containerCart}>
-          <img className={style.cartImage} src="" alt="cart"/>
+          <FontAwesomeIcon className={style.cartImage} icon={faCartShopping} />
           <p className={style.itemsInCart}>8</p>
         </div>
 
@@ -98,7 +143,7 @@ function ClientProfile(){
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default ClientProfile;

3b5e9f0bca6c7a1e24fe4d99cd2e584dc31ea538
Esneider Ospina <sneuder8@gmail.com>
2022-04-21 02:06:56 -0500

    Fixed

--- client/src/components/Product_detail/Product_detail.jsx
+++ client/src/components/Product_detail/Product_detail.jsx
@@ -49,6 +49,7 @@ export default function Product_detail() {
 
   const { name, brand, price, description } = product;
 
+
   const [state, setState] = useState();
 
   useEffect(() => {
@@ -164,113 +165,103 @@ export default function Product_detail() {
     }
   }
 
-	function handleFavorites(e) {
-		e.preventDefault();
-		if (client?.phone) {
-			if (favorite.length) {
-				console.log(favorite);
-				if (
-					favorite[0]?.List?.map((p) =>
-						parseInt(p.id_product)
-					).includes(parseInt(id))
-				) {
-					swal(
-						'This product already is on favorites',
-						'Click to continue!',
-						'warning'
-					);
-				} else {
-					const listUpdated = {
-						id: favorite[0]?.id,
-						ClientPhone: parseInt(favorite[0]?.ClientPhone),
-						rList: [
-							...favorite[0]?.List?.map((p) => p.id_product),
-							parseInt(id),
-						],
-						Colaborators: [...favorite[0].Colaborators],
-						title: favorite[0]?.title,
-					};
-					dispatch(updateList(listUpdated));
-					swal(
-						'Product added to your list',
-						'Click to continue!',
-						'success'
-					);
-				}
-			} else {
-				const newList = {
-					ClientPhone: client.phone,
-					rList: [parseInt(id)],
-					Colaborators: [],
-					title: 'Favorite',
-				};
-				dispatch(createList(newList, swal));
-			}
-		} else {
-			swal({
-				title: 'You have to be logged in to add products',
-				text: 'Would you like to login?',
-				icon: 'warning',
-				buttons: true,
-				dangerMode: true,
-			}).then((willAccept) => {
-				if (willAccept) {
-					prepareProduct(product);
-					params.push('/login');
-				} else {
-				}
-			});
-		}
-	}
+  function handleFavorites(e) {
+    e.preventDefault();
+    if (client?.phone) {
+      if (favorite.length) {
+        console.log(favorite);
+        if (
+          favorite[0]?.List?.map((p) => parseInt(p.id_product)).includes(
+            parseInt(id)
+          )
+        ) {
+          swal(
+            "This product already is on favorites",
+            "Click to continue!",
+            "warning"
+          );
+        } else {
+          const listUpdated = {
+            id: favorite[0]?.id,
+            ClientPhone: parseInt(favorite[0]?.ClientPhone),
+            rList: [
+              ...favorite[0]?.List?.map((p) => p.id_product),
+              parseInt(id),
+            ],
+            Colaborators: [...favorite[0].Colaborators],
+            title: favorite[0]?.title,
+          };
+          dispatch(updateList(listUpdated));
+          swal("Product added to your list", "Click to continue!", "success");
+        }
+      } else {
+        const newList = {
+          ClientPhone: client.phone,
+          rList: [parseInt(id)],
+          Colaborators: [],
+          title: "Favorite",
+        };
+        dispatch(createList(newList, swal));
+      }
+    } else {
+      swal({
+        title: "You have to be logged in to add products",
+        text: "Would you like to login?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      }).then((willAccept) => {
+        if (willAccept) {
+          prepareProduct(product);
+          params.push("/login");
+        } else {
+        }
+      });
+    }
+  }
 
-	if (!product?.variants) return <Loader />;
-	console.log(product);
-	return (
-		<div className={style.container}>
-			<div className={style.containerImages}>
-				<div className={style.containerMainImage}>
-					{product?.is_offer && (
-						<span className={style.offer}>{`-${(
-							(100 * (-product.price + product.price_offer)) /
-							product.price
-						).toFixed(0)}%`}</span>
-					)}
-					<img
-						className={style.mainImage}
-						src={
-							product?.variants &&
-							product?.variants[0]?.ProductImages[0]
-						}
-						id='default_image'
-						alt=''
-					/>
-				</div>
-				<div className={style.containerSecondImages}>
-					{product?.variants &&
-						product?.variants[0]?.ProductImages.map((image) => (
-							<img
-								key={image}
-								className={style.secondImages}
-								src={image}
-								alt=''
-								onClick={() => {
-									selectImage(image);
-								}}
-							/>
-						))}
-				</div>
-			</div>
+  if (!product?.variants) return <Loader />;
+  console.log(product);
+  return (
+    <div className={style.newContainer}>
+      <div className={style.container}>
+        <div className={style.containerImages}>
+          <div className={style.containerMainImage}>
+            {product?.is_offer && (
+              <span className={style.offer}>{`-%${
+                (100 * (-product.price + product.price_offer)) / product.price
+              }`}</span>
+            )}
+            <img
+              className={style.mainImage}
+              src={product?.variants && product?.variants[0]?.ProductImages[0]}
+              id="default_image"
+              alt=""
+            />
+          </div>
+          <div className={style.containerSecondImages}>
+            {product?.variants &&
+              product?.variants[0]?.ProductImages.map((image) => (
+                <img
+                  key={image}
+                  className={style.secondImages}
+                  src={image}
+                  alt=""
+                  onClick={() => {
+                    selectImage(image);
+                  }}
+                />
+              ))}
+          </div>
+        </div>
 
-			<div className={style.containerInf}>
-				<div className={style.specificInf}>
-					<h2 className={style.productName}>{name}</h2>
-					<p className={style.collectionName}>{brand}</p>
-					<p
-						className={style.productPrice}
-						id='individualProductPrice'>{`$${
-						parseFloat(product?.totalPrice).toFixed(2) ||
-						product?.price?.toFixed(2)
-					}`}</p>
+        <div className={style.containerInf}>
+          <div className={style.specificInf}>
+            <h2 className={style.productName}>{name}</h2>
+            <p className={style.collectionName}>{brand}</p>
+            <p className={style.productPrice} id="individualProductPrice">{`$${
+              product?.totalPrice || product?.price
+            }`}</p>
 
             <div className={style.containerPreferences}>
               <div className={style.containerSizePreference}>
@@ -413,13 +404,10 @@ export default function Product_detail() {
               let { login_name } = review.Client;
               let stars = Array.from(Array(review.stars).keys());
 
-              // stars = parseInt(stars);
-
               return (
                 <div className={style.containerReview}>
                   <img className={style.profile} src="https://prephoopsnext.com/wp-content/themes/prepsports/resources/assets/images/default-user.png"/>
                   <div className={style.contentReview}>
-                    <h3 className={style.login_nameReviewer}>{login_name}</h3>
                     <div className={style.containerStars}>
                       {
                         stars.map((star) => {
@@ -427,6 +415,7 @@ export default function Product_detail() {
                         })
                       }
                     </div>
+                    <h3 className={style.login_nameReviewer}>{login_name}</h3>
                     <p className={style.textReview}>{description}</p>
                   </div>
                 </div>
